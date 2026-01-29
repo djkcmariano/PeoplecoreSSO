54a7bd313fcad7ef658a234e2cfb6f125425d6df
dcmariano <marianodeejann@gmail.com>
2026-01-29 14:07:50 +0800

    Update from local

--- obj/Debug/net8.0/AuthServer.AssemblyInfo.cs
+++ obj/Debug/net8.0/AuthServer.AssemblyInfo.cs
@@ -11,11 +11,10 @@
 using System;
 using System.Reflection;
 
-[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("4ffe4b61-e5de-431d-87ce-bb04edde7642")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd7988dbd070effc7a98fff3508a678f823637df")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ccf139642c670ba89cece808bb4e207d514660c")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
